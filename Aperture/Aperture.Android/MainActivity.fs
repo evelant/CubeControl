cf0967ad5ff19a2f94f96e741d8f8f1249f08268
evelant <andrew@taskheroics.com>
2024-08-31 14:08:01 -0400

    Initial commit. Basic project setup. Fabulous.Avalonia UI template projects that compile and run on all platforms with .net9 preview.

---
+++ Aperture/Aperture.Android/MainActivity.fs
@@ -0,0 +1,18 @@
+﻿namespace Aperture.Android
+
+open Android.App
+open Android.Content.PM
+open Avalonia
+open Avalonia.Android
+open Fabulous.Avalonia
+open Aperture
+
+[<Activity(Label = "Aperture.Android",
+           Theme = "@style/MyTheme.NoActionBar",
+           Icon = "@drawable/icon",
+           LaunchMode = LaunchMode.SingleTop,
+           ConfigurationChanges = (ConfigChanges.Orientation ||| ConfigChanges.ScreenSize))>]
+type MainActivity() =
+    inherit AvaloniaMainActivity<FabApplication>()
+
+    override this.CustomizeAppBuilder(_builder: AppBuilder) = App.create().UseAndroid()
---
+++ Aperture/Aperture.Browser/Program.fs
@@ -0,0 +1,59 @@
+// open System.Runtime.Versioning
+// open Avalonia
+// open Avalonia.Browser
+// #if (ReactiveUIToolkitChosen)
+// open Avalonia.ReactiveUI
+// #endif
+// open AvaloniaTest
+//
+// module Program =
+//     [<assembly: SupportedOSPlatform("browser")>]
+//     do ()
+//
+//     [<CompiledName "BuildAvaloniaApp">] 
+//     let buildAvaloniaApp () = 
+//         AppBuilder
+//             .Configure<App>()
+//
+//     [<EntryPoint>]
+//     let main argv =
+//         task {
+//             do! (buildAvaloniaApp()
+//             .WithInterFont()
+// #if (ReactiveUIToolkitChosen)
+//             .UseReactiveUI()
+// #endif
+//             .StartBrowserAppAsync("out"))
+//         }
+//         |> ignore
+//         0
+
+namespace FabAvaloniaTemplate.Browser
+
+open System.Diagnostics
+open System.Runtime.Versioning
+open Avalonia
+open Avalonia.Browser
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Logging
+open Avalonia.Threading
+open Aperture
+
+
+module Program =
+    [<assembly: SupportedOSPlatform("browser")>]
+    do ()
+
+    [<CompiledName "BuildAvaloniaApp">]
+    let buildAvaloniaApp () = Aperture.App.create()
+
+    [<EntryPoint>]
+    let main argv =
+        Trace.Listeners.Add(new ConsoleTraceListener()) |> ignore
+
+        buildAvaloniaApp()
+            .LogToTrace(LogEventLevel.Warning)
+            .StartBrowserAppAsync("out", BrowserPlatformOptions())
+        |> ignore
+
+        0
